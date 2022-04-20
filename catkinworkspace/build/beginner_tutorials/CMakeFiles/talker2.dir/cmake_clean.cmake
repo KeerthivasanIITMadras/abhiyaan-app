78265b6b4861fa94362806da1e8c9999da86fba8
keerthivasan <keerthivasan.de.villiers.17@gmail.com>
2022-04-20 23:18:25 +0530

    file upload

---
+++ catkinworkspace/build/beginner_tutorials/CMakeFiles/talker2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/keerthivasan/catkinworkspace/devel/lib/beginner_tutorials/talker2"
+  "/home/keerthivasan/catkinworkspace/devel/lib/beginner_tutorials/talker2.pdb"
+  "CMakeFiles/talker2.dir/src/talker2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
